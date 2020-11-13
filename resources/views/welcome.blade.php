5cf6a444b96d1b0485133df8d59e650563a80ca0
devdreamsolution <q3construction1@gmail.com>
2020-11-14 00:51:44 +1000

    social buttons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -130,10 +130,9 @@
 
             <div id="social-links">
                 <ul>
-                    <li><a href="https://www.facebook.com/sharer/sharer.php?u=http://jorenvanhocht.be" class="social-button " id=""><span class="fa fa-facebook-official"></span></a></li>
-                    <li><a href="https://twitter.com/intent/tweet?text=my share text&amp;url=http://jorenvanhocht.be" class="social-button " id=""><span class="fa fa-twitter"></span></a></li>
-                    <li><a href="http://www.linkedin.com/shareArticle?mini=true&amp;url=http://jorenvanhocht.be&amp;title=my share text&amp;summary=dit is de linkedin summary" class="social-button " id=""><span class="fa fa-linkedin"></span></a></li>
-                    <li><a href="https://wa.me/?text=http://jorenvanhocht.be" class="social-button " id=""><span class="fa fa-whatsapp"></span></a></li>    
+                    <li><a href="https://www.facebook.com/sharer/sharer.php?u=https://spscloud.io/competition-rules-and-terms" class="social-button " id=""><span class="fa fa-facebook-official"></span></a></li>
+                    <li><a href="https://twitter.com/intent/tweet?url=https://spscloud.io/competition-rules-and-terms" class="social-button " id=""><span class="fa fa-twitter"></span></a></li>
+                    <li><a href="https://www.instagram.com/bad_hat_films/" class="social-button " id=""><span class="fa fa-instagram"></span></a></li>
                 </ul>
             </div>
         </div>
