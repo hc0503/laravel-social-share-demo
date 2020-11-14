384dcddeb4dcff82ebcb752f629b12da1ed429f6
devdreamsolution <q3construction1@gmail.com>
2020-11-15 00:10:08 +1000

    add meta for social iamge and content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,18 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="SPSCloud Screenwriting competition" />
+        <meta property="og:description" content="I have just entered the SPSCloud FREE screenwriting competition." />
+        <meta property="og:site_name" content="SPSCloud" />
+        <meta property="article:published_time" content="2020-11-13T06:24:02+00:00" />
+        <meta property="og:image" content="https://spscloud.io/wp-content/uploads/2020/11/spsc-trophy-208x300.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:label1" content="Written by">
+        <meta name="twitter:data1" content="David Raynor">
+        <meta name="twitter:label2" content="Est. reading time">
+        <meta name="twitter:data2" content="1 minute">
         <title>Laravel</title>
 
         <!-- Fonts -->
@@ -130,8 +141,8 @@
 
             <div id="social-links">
                 <ul>
-                    <li><a href="https://www.facebook.com/sharer/sharer.php?u=https://spscloud.io/competition-rules-and-terms" class="social-button " id=""><span class="fa fa-facebook-official"></span></a></li>
-                    <li><a href="https://twitter.com/intent/tweet?url=https://spscloud.io/competition-rules-and-terms" class="social-button " id=""><span class="fa fa-twitter"></span></a></li>
+                    <li><a href="https://www.facebook.com/sharer/sharer.php?u=https://app.spscloud.io" class="social-button " id=""><span class="fa fa-facebook-official"></span></a></li>
+                    <li><a href="https://twitter.com/intent/tweet?url=https://app.spscloud.io/competition-rules-and-terms" class="social-button " id=""><span class="fa fa-twitter"></span></a></li>
                     <li><a href="https://www.instagram.com/bad_hat_films/" class="social-button " id=""><span class="fa fa-instagram"></span></a></li>
                 </ul>
             </div>
